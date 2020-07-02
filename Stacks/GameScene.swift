4340e6cd6fbdbaf476b84359fcb99d79ea18cde4
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-07-02 08:45:50 -0700

    Game done

--- Stacks/GameScene.swift
+++ Stacks/GameScene.swift
@@ -16,16 +16,57 @@ struct PhysicsCategory {
     static let Barrier: UInt32 = 0b1000
 }
 
-class GameScene: SKScene {
+enum GameState: Equatable {
+    case Active
+    case Menu
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
     let scrollSpeed: CGFloat = 200
     var scrollNode: SKNode!
     var cloudScroll: SKNode!
-    var player: SKSpriteNode!
+    var player: Player!
     var obstacleSpawner: ObstacleSpawner!
     var playButton: CustomButtonNode!
     var frontBarrier: SKSpriteNode!
+    var obstacleTimer: Timer!
+    var gameState: GameState = .Menu {
+        didSet{
+            switch gameState {
+            case .Active:
+                // for the nodes in bodyNodes we well remove all of them
+                for node in player.bodyNodes {
+                    node.removeFromParent()
+                }
+                // remove all animations from the player
+                player.removeAllActions()
+                //reset the bodyNodes array
+                self.player.bodyNodes = []
+                // reset the rotation to make sure its facing the correct way.
+                self.player.zRotation = 0
+                // hide the play button
+                self.playButton.isHidden = true
+                // reset the player location
+                self.player.position.x = self.player.initialPos.x
+                // start a timer by assigning a new one.
+                self.obstacleTimer = Timer.scheduledTimer(timeInterval: self.fixedDelta, target: self, selector: #selector(self.startGenerator), userInfo: nil, repeats: true)
+            case .Menu:
+                // show the play button
+                self.playButton.isHidden = false
+                // stop the timer
+                self.obstacleTimer.invalidate()
+
+                // for all nodes that are an obstacle remove them from the scene
+                for node in self.children {
+                    if node.physicsBody?.categoryBitMask == PhysicsCategory.Obstacle {
+                        node.removeFromParent()
+                    }
+                }
+            }
+        }
+    }
     
     override func sceneDidLoad() {
         super.sceneDidLoad()
@@ -60,15 +101,36 @@ class GameScene: SKScene {
         } else {
             print("spawner could not be connected properly")
         }
+        
+        if let player = self.childNode(withName: "player") as? Player {
+            self.player = player
+        } else {
+            print("player was not initialized properly")
+        }
+        player.setup()
 
         // setting the barrier physics body preferences
         frontBarrier.physicsBody?.categoryBitMask = PhysicsCategory.Barrier
         frontBarrier.physicsBody?.collisionBitMask = PhysicsCategory.None
         frontBarrier.physicsBody?.contactTestBitMask = PhysicsCategory.Obstacle
+        
+        playButton.selectedHandler = {
+            self.gameState = .Active
+        }
+        
+        physicsWorld.contactDelegate = self
+    }
+    
+    @objc func startGenerator(){
+        self.obstacleSpawner.generate(scene: self.scene!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        // ray that detects other nodes above the player
+        let nodeCheck = physicsWorld.body(alongRayStart: player.position, end: CGPoint(x: player.position.x, y: player.position.y + 100))
+        if nodeCheck?.node == nil {
+            player.stack(scene: scene!)
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -76,6 +138,11 @@ class GameScene: SKScene {
         
         /* Process world scrolling */
         scrollWorld()
+        
+        if gameState == .Active {
+            checkBody()
+            checkPlayer()
+        }
         
     }
     
@@ -118,4 +185,58 @@ class GameScene: SKScene {
             }
         }
     }
+    
+    func checkPlayer() {
+        // if the player gets bumped back past the error margin of 10 we play an animation and change the game state
+        if player.position.x < player.initialPos.x - 10{
+            // animations
+            let rotate = SKAction.rotate(byAngle: 15, duration: 2.5)
+            let pushBack = SKAction.moveTo(x: player.position.x - 400, duration: 2)
+            let seq = SKAction.group([pushBack, rotate])
+            player.run(seq)
+            gameState = .Menu
+        } else {
+            // else if the player gets bumped an insignificant amount we just reset the play back to the original spot.
+            self.player.position.x = self.player.initialPos.x
+        }
+    }
+    
+    func checkBody(){
+        // if we actually have nodes to check if they have been moved back more then the error margin of 5 we play an animation and remove them from the scene
+        if player.bodyNodes.count >= 1 {
+            for sprite in player.bodyNodes {
+                if sprite.position.x < player.position.x - 5 {
+                    // filter out the current node that is past the margin of error
+                    player.bodyNodes = player.bodyNodes.filter {return $0 != sprite }
+                    // animations
+                    let rotate = SKAction.rotate(byAngle: 20, duration: 2)
+                    let pushBack = SKAction.moveTo(x: sprite.position.x - 400, duration: 2)
+                    // remove the node after the animation is done
+                    let remove = SKAction.run {
+                        sprite.removeFromParent()
+                    }
+                    let group = SKAction.group([pushBack, rotate])
+                    let seq = SKAction.sequence([group, remove])
+                    sprite.run(seq)
+                }
+            }
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        // check the categoryBitMasks to make sure we are removing the correct node.
+        if bodyA.categoryBitMask == PhysicsCategory.Barrier {
+            if bodyB.categoryBitMask == PhysicsCategory.Obstacle {
+                bodyB.node?.removeFromParent()
+            }
+        }
+        if bodyB.categoryBitMask == PhysicsCategory.Barrier {
+            if bodyA.categoryBitMask == PhysicsCategory.Obstacle {
+                bodyA.node?.removeFromParent()
+            }
+        }
+    }
+    
 }
